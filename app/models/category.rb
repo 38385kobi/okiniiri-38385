5d8a2d6e5a90dcadcc9298b4a271c24cc1b75d41
38385kobi <111612810+38385kobi@users.noreply.github.com>
2022-10-07 21:17:12 +0900

    商品一覧機能

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
-class CategoryId < ActiveHash::Base
+class Category < ActiveHash::Base
   self.data = [
     {id: 0, name: '---'}, {id: 1, name: '帽子'}, {id: 2, name: 'バック・カバン'},
     {id: 3, name: 'アクセサリー'}, {id: 4, name: '小物'}, {id: 5, name: 'インテリア雑貨'},
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :delivery_date
   belongs_to :user
   has_one_attached :image
 

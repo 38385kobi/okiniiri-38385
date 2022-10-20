1a77f8cd0ce003ff3ca5c7bfe43a76d8d163235f
38385kobi <111612810+38385kobi@users.noreply.github.com>
2022-10-20 22:26:41 +0900

    ActionCableを本番環境で使用する設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "wss://okiniiri-38385.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://okiniiri-38385.herokuapp.com', 'http://okiniiri-38385.herokuapp.com']
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
